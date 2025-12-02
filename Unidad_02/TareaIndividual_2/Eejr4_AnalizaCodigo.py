79ec577910ad86fc37631aa7bd97851e8358f7ea
ManuFer15 <manu06042004@gmail.com>
2025-12-02 21:00:57 +0100

    commit Tarea individiual 2

---
+++ Unidad_02/TareaIndividual_2/Eejr4_AnalizaCodigo.py
@@ -0,0 +1,14 @@
+'''
+num = input("Introduce un número: ")
+
+if num % 2 = 0:
+    print("Es par")
+else
+    print("Es impar")
+'''
+num = int(input("Introduce un número: ")) #Faltaba convertir a entero
+
+if num % 2 == 0: #Le fataba otro igual
+    print("Es par")
+else: #Le faltaba dos puntos
+    print("Es impar")
---
+++ Unidad_02/TareaIndividual_2/Ejer5_Multiplo3.py
@@ -0,0 +1,5 @@
+num = int(input("Introduce un número: "))
+if num % 3 == 0:
+    print(f"El número {num} es múltiplo de 3")
+else:
+    print(f"El número {num} no es múltiplo de 3")
