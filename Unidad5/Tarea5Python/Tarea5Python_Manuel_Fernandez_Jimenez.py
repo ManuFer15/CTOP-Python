860cb3e0963e2d83ffa04610631e4ee8f565cc2a
ManuFer15 <manu06042004@gmail.com>
2026-02-02 19:12:00 +0100

    Tarea 5 finalizada

--- Unidad5/Tarea5Python/Tarea5Python_Manuel_Fernandez_Jimenez.py
+++ Unidad5/Tarea5Python/Tarea5Python_Manuel_Fernandez_Jimenez.py
@@ -17,10 +17,9 @@ print(info_producto(prod2))
 
 ##RA05_B:
 #Ejer2:
-class ProductoPrivado:
+class ProductoPrivado(Producto):
     def __init__(self, id, nombre, precio):
-        self.id = id
-        self.nombre = nombre
+        super().__init__(id, nombre, precio)
         self.__precio = precio
 
     def get_precio(self):
@@ -44,3 +43,68 @@ print(info_producto_privado(prod4))
 
 ##RA05_C:
 #Ejer3:
+class ProductoAlimenticio(Producto):
+    def __init__(self, id, nombre, precio, fechaCaducidad):
+        super().__init__(id, nombre, precio)
+        self.fechaCaducidad = fechaCaducidad
+
+    def comprobarCaducidad(self, fechaActual):
+        if fechaActual < self.fechaCaducidad:
+            return "No caducado"
+        else:
+            return "Caducado"
+pAli1 = ProductoAlimenticio(5, "Leche", 1.20, "2024-12-01")
+pAli2 = ProductoAlimenticio(6, "Yogur", 0.80, "2024-05-15")
+print("\nRA05_C: Ejer3")
+print(f"Producto: {pAli1.nombre}, Estado: {pAli1.comprobarCaducidad('2026-06-01')}")
+print(f"Producto: {pAli2.nombre}, Estado: {pAli2.comprobarCaducidad('2026-06-01')}")
+
+
+##RA05_D:
+#Ejer4:
+#import sqlite3
+
+#conn = sqlite3.connect('productos.db')
+#cursor = conn.cursor()
+#cursor.execute('''CREATE TABLE IF NOT EXISTS productos (id INTEGER PRIMARY KEY, nombre TEXT, precio REAL, fechaCaducidad TEXT)''')
+
+##RA05_E:
+#Ejer5:
+#cursor.execute("INSERT INTO productos (id, nombre, precio, fechaCaducidad) VALUES (?, ?, ?, ?)", (id, "pan", "0.50", "2026-06-10"))
+#conn.commit()
+#cursor.execute("SELECT * FROM productos")
+#cursor.fetchall()
+#cursor.close()
+#conn.close()
+
+##RA05_F:
+#Ejer6:
+import sqlite3
+class GestorBD:
+    def __init__(self, db_name='productos.db'):
+        self.conn = sqlite3.connect(db_name)
+
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS productos (id INTEGER PRIMARY KEY, nombre TEXT, precio REAL, fechaCaducidad TEXT)''')
+
+    def agregar_producto(self, producto):
+        self.cursor.execute("INSERT INTO productos (id, nombre, precio, fechaCaducidad) VALUES (?, ?, ?, ?)",
+                            (producto.id, producto.nombre, producto.precio, getattr(producto, 'fechaCaducidad', None)))
+        self.conn.commit()
+
+    def obtener_productos(self):
+        self.cursor.execute("SELECT * FROM productos")
+        return self.cursor.fetchall()
+
+    def cerrar(self):
+        self.cursor.close()
+        self.conn.close()
+
+gestor = GestorBD()
+gestor.agregar_producto(pAli1)
+gestor.agregar_producto(pAli2)
+productos = gestor.obtener_productos()
+print("\nRA05_F: Ejer6")
+for prod in productos:
+    print(prod)
+gestor.cerrar()
